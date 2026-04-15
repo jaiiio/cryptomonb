70ca33164b2e8e1fff0dd395e0c9435fd26c9373
jaiiio <fabiofdz@gmail.com>
2026-04-15 09:25:51 -0300

    update

--- main.py
+++ main.py
@@ -63,11 +63,11 @@ def monitorear():
 
     escribir_precio(timestamp, data)
 
-schedule.every(1).minutes.do(monitorear)
+schedule.every(20).minutes.do(monitorear)
 
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(30)
 
 # #Carga env
 # load_dotenv()
